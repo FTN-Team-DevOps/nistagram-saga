a69b4c8fa81f77a592721aa4d34db8dcb619933a
unknown <filip97@gmail.com>
2021-07-26 14:43:12 +0200

    test file

---
+++ src/publication/publication.controller.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PublicationController } from './publication.controller';
+import { PublicationService } from './publication.service';
+
+describe('PublicationController', () => {
+  let controller: PublicationController;
+
+  // const mockPublicationService = {
+  //   test: jest.fn(() => {
+  //     return 'test';
+  //   }),
+  // };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PublicationController],
+      providers: [PublicationService],
+    })
+      // .overrideProvider(PublicationService)
+      // .useValue(mockPublicationService)
+      .compile();
+
+    controller = module.get<PublicationController>(PublicationController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  // it('should return "test"', () => {
+  //   expect(() => publicationController.search()).toBe('test');
+  // });
+});
